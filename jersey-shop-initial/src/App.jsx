93625f74eb98a74b5d7f860f5901264f3b15348b
Potatoyz908 <euller2005@gmail.com>
2025-01-07 10:45:34 -0300

    adiciona configuração inicial do projeto com Vite e React

---
+++ jersey-shop-initial/src/App.jsx
@@ -0,0 +1,119 @@
+import './App.css';
+import OrderDetails from './components/OrderDetails';
+import Item from './components/Item';
+
+function App() {
+
+    const items = [
+        
+        {
+            id: 2, 
+            photo: "milan.png",
+            name: "Milan",
+            price: 99.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },   
+        {
+            id: 3, 
+            photo: "chelsea.webp",
+            name: "Chelsea",
+            price: 99.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 4, 
+            photo: "barcelona.png",
+            name: "Barcelona",
+            price: 109.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 5, photo: "benfica.png",
+            name: "Benfica",
+            price: 89.49,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 6, 
+            photo: "manchester.webp",
+            name: "Manchester City",
+            price: 129.79,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 7, 
+            photo: "bayern.webp",
+            name: "Bayern",
+            price: 119.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 8, 
+            photo: "psg.png",
+            name: "PSG",
+            price: 94.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        },
+        {
+            id: 9, 
+            photo: "ajax.webp",
+            name: "Ajax",
+            price: 89.99,
+            active: false,
+            quantity: 1, 
+            isInBag: false
+        }
+    ];
+
+    const itemsInBag = items.filter(item => item.isInBag);
+
+    return ( 
+        <>
+            <section className="items">
+                <h4>Euller Shop Made with React JS</h4>
+
+                {items.map((item) => 
+                    <div key= {item.id} className={`product ${item.isInBag ? "selected" : ""}`}>
+                        <div className="photo">
+                            <img src={"./img/" + item.photo} /> 
+                        </div>
+                        <div className="description">
+                            <span className="name">{item.name}</span>
+                            <span className="price">{item.price}</span>
+                            {
+
+                                item.isInBag && 
+                                <div className="quantity-area">
+                                <button>-</button>
+                                <span className="quantity">{item.quantity}</span>
+                                <button>+</button>
+                            </div>
+                        
+                            }
+                        </div>
+                    </div>)}
+                
+                
+            </section>
+            
+            {itemsInBag.length > 0 && <OrderDetails />}
+            
+        </>
+    );
+}
+
+export default App
